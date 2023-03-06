d22ce713d3d2d118e104bae30733c02fc811e957
matthew <larochelle.matthew@gmail.com>
2023-03-06 15:12:41 -0800

    tech stack done

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,22 +1,65 @@
 import React from "react";
 import styles from './Tech.module.css'
+import {FaReact, FaHtml5, FaCss3, FaBootstrap, FaGithub, FaDocker, FaNodeJs, FaPython} from 'react-icons/fa';
+import {IoLogoJavascript} from 'react-icons/io';
+import {SiMongodb, SiMysql} from 'react-icons/si';
 
 function Tech() {
     return (
         <div>
             <div className={styles.tech}>
-                <h3 className={styles.title}>My tech stack</h3>
+                <h3 className={styles.title}>My Tech Stack</h3>
                 <div className={styles.stack}>
-                <i class="fa-brands fa-react"></i>
-                <i class="fa-brands fa-node-js"></i>
-                <i class="fa-brands fa-html5"></i>
-                <i class="fa-brands fa-css3"></i>
-                <i class="fa-brands fa-square-js"></i>
-                <i class="fa-brands fa-bootstrap"></i>
-                <i class="fa-brands fa-github"></i>
-                <i class="fa-brands fa-docker"></i>
-                <i class="fa-brands fa-python"></i>
-                <p>Mongo DB and SQL</p>
+                    <div className={styles.row1}>
+                        <div className={styles.item}>
+                            <FaReact />
+                            <p>React</p>
+                        </div>
+                        <div className={styles.item}>
+                            <IoLogoJavascript />
+                            <p>JavaScript</p>
+                        </div>
+                        <div className={styles.item}>
+                            <FaHtml5 />
+                            <p>HTML</p>
+                        </div>
+                        <div className={styles.item}>
+                            <FaCss3 />
+                            <p>CSS</p>
+                        </div>
+                    </div>
+                    <div className={styles.row2}>
+                        <div className={styles.item}>
+                            <FaBootstrap />
+                            <p>Bootstrap</p>
+                        </div>
+                        <div className={styles.item}>
+                            <SiMongodb />
+                            <p>MongoDB</p>
+                        </div>
+                        <div className={styles.item}>
+                            <SiMysql />
+                            <p>MySQL</p>
+                        </div>
+                        <div className={styles.item}>
+                            <FaPython />
+                            <p>Python</p>
+                        </div>
+                    </div>
+                    <div className={styles.row3}>
+                        <div className={styles.item}>
+                            <FaGithub />
+                            <p>GitHub</p>
+                        </div>
+                        <div className={styles.item}>
+                            <FaDocker />
+                            <p>Docker</p>
+                        </div>
+                        <div className={styles.item}>
+                            <FaNodeJs />
+                            <p>NodeJS</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
