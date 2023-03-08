951c8e4c52f2e6043959905b7f045660467bd53d
matthew <larochelle.matthew@gmail.com>
2023-03-07 16:14:30 -0800

    edit swiper styles

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -23,38 +23,53 @@ function Projects() {
                 modules={[Pagination, Navigation]}
                 className={styles.container}
             >
-                <SwiperSlide className={styles.project}>
-                    <div className={styles.project_content}>
-                        <h3>Brandywine Carriage Tours</h3>
-                        <p>A website I built for a horse drawn carriage service</p>
-                        <p><FaReact /> <IoLogoJavascript /> <FaHtml5 /> <FaCss3 /></p>
-                    </div>
-                    <div className={styles.project_image}>
-                        <img src={ImgCarriage} alt='horse-carriage website' />
-                    </div>
-                    <div className={styles.links}>
-                        <button><a href='https://moonlit-shortbread-27d65b.netlify.app'>Link</a></button>
-                        <button><a href='https://github.com/Matt-LaRochelle/carriage'>Code</a></button>
+                <SwiperSlide>
+                    <div className={styles.project}>
+                        <div className={styles.project_content}>
+                            <h3>Brandywine Carriage Tours</h3>
+                            <p>A website I built for a horse drawn carriage service</p>
+                            <h3>Challenges faced:</h3>
+                            <ul>
+                                <li>Learning sliderJS</li>
+                                <li>Understanding CSS modules</li>
+                                <li>Practice with flexbox</li>
+                                <li>Creating Hero sections</li>
+                                <li>Creating a custom contact form</li>
+                            </ul>
+                            <h3>Technologies used:</h3>
+                            <p><FaReact /> <IoLogoJavascript /> <FaHtml5 /> <FaCss3 /></p>
+                            <div className={styles.links}>
+                                <button><a href='https://moonlit-shortbread-27d65b.netlify.app'>Link</a></button>
+                                <button><a href='https://github.com/Matt-LaRochelle/carriage'>Code</a></button>
+                            </div>
+                        </div>
+                        <div className={styles.project_image}>
+                            <img src={ImgCarriage} alt='horse-carriage website' />
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className={styles.project}>
-                    <h3>Project 2</h3>
-                    <p>A website for a horseback riding school</p>
-                    <p>Include an image</p>
-                    <p>Include a link</p>
+                <SwiperSlide>
+                    <div className={styles.project}>
+                        <h3>Project 2</h3>
+                        <p>A website for a horseback riding school</p>
+                        <p>Include an image</p>
+                        <p>Include a link</p>
+                    </div>
                 </SwiperSlide>
-                <SwiperSlide className={styles.project}>
-                    <div className={styles.project_content}>
-                        <h3>Portfolio website</h3>
-                        <p>A website built to track my projects and show to potential clients</p>
-                        <p><FaReact /> <IoLogoJavascript /> <FaHtml5 /> <FaCss3 /></p>
-                    </div>
-                    <div className={styles.project_image}>
-                        <img src={Personal} alt="my personal website" />
-                    </div>
-                    <div className={styles.links}>
-                        <button><a href='https://rad-meringue-4511ee.netlify.app/'>Link</a></button>
-                        <button><a href='https://github.com/Matt-LaRochelle/matt-personal'>Code</a></button>
+                <SwiperSlide>
+                    <div className={styles.project}>
+                        <div className={styles.project_content}>
+                            <h3>Portfolio website</h3>
+                            <p>A website built to track my projects and show to potential clients</p>
+                            <p><FaReact /> <IoLogoJavascript /> <FaHtml5 /> <FaCss3 /></p>
+                            <div className={styles.links}>
+                                <button><a href='https://rad-meringue-4511ee.netlify.app/'>Link</a></button>
+                                <button><a href='https://github.com/Matt-LaRochelle/matt-personal'>Code</a></button>
+                            </div>
+                        </div>
+                        <div className={styles.project_image}>
+                            <img src={Personal} alt="my personal website" />
+                        </div>
                     </div>
                 </SwiperSlide>
             </Swiper>
