048a6f8a00c7e2e217e73af585106c6d6d0f6bdc
matthew <larochelle.matthew@gmail.com>
2023-04-06 16:39:09 -0700

    add new project and update norman picture

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,7 +7,8 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import ImgCarriage from '../images/carriage_pic.png';
 import Personal from '../images/personal.png';
-import Norman from '../images/Norman.png';
+import Norman from '../images/norman.png';
+import l2 from '../images/l2.png';
 import { FaReact, FaHtml5, FaCss3 } from 'react-icons/fa';
 import {IoLogoJavascript} from 'react-icons/io';
 
@@ -23,31 +24,6 @@ function Projects() {
                 modules={[Pagination, Navigation]}
                 className={styles.container}
             >
-                <SwiperSlide>
-                    <div className={styles.project}>
-                        <div className={styles.project_content}>
-                            <h3>Brandywine Carriage Tours</h3>
-                            <p>A website I built for a horse drawn carriage service</p>
-                            <h3>Challenges faced:</h3>
-                            <ul>
-                                <li>Learning swiperJS</li>
-                                <li>Understanding CSS modules</li>
-                                <li>Practice with flexbox</li>
-                                <li>Creating Hero sections</li>
-                                <li>Creating a custom contact form</li>
-                            </ul>
-                            <h3>Technologies used:</h3>
-                            <p><FaHtml5 /> <FaCss3 /> <IoLogoJavascript /> <FaReact /></p>
-                            <div className={styles.links}>
-                                <a href='https://moonlit-shortbread-27d65b.netlify.app'><button>Link</button></a>
-                                <a href='https://github.com/Matt-LaRochelle/carriage'><button>Code</button></a>
-                            </div>
-                        </div>
-                        <div className={styles.project_image}>
-                            <img src={ImgCarriage} alt='horse-carriage website' />
-                        </div>
-                    </div>
-                </SwiperSlide>
                 <SwiperSlide>
                     <div className={styles.project}>
                         <div className={styles.project_content}>
@@ -70,6 +46,56 @@ function Projects() {
                         </div>
                         <div className={styles.project_image}>
                             <img src={Norman} alt="my personal website" />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={styles.project}>
+                        <div className={styles.project_content}>
+                            <h3>Windmill Equestrian</h3>
+                            <p>A website for a horse back riding school</p>
+                            <h3>Challenges faced</h3>
+                            <ul>
+                                <li>Limiting line length to 75 characters</li>
+                                <li>Creating dynamic CSS for viewport size</li>
+                                <li>Experimenting with different navbar styles</li>
+                                <li>Creating a simple but professional design</li>
+                                <li>Finding other solutions for contact functionality</li>
+                            </ul>
+                            <h3>Technologies used:</h3>
+                            <p><FaHtml5 /> <FaCss3 /> <IoLogoJavascript /> <FaReact /></p>
+                            <div className={styles.links}>
+                                <a href='https://gregarious-gumption-b9353e.netlify.app/'><button>Link</button></a>
+                                <a href='https://github.com/Matt-LaRochelle/horses'><button>Code</button></a>
+                            </div>
+                        </div>
+                        <div className={styles.project_image}>
+                            <img src={l2} alt="my personal website" />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={styles.project}>
+                        <div className={styles.project_content}>
+                            <h3>Brandywine Carriage Tours</h3>
+                            <p>A website I built for a horse drawn carriage service</p>
+                            <h3>Challenges faced:</h3>
+                            <ul>
+                                <li>Learning swiperJS</li>
+                                <li>Understanding CSS modules</li>
+                                <li>Practice with flexbox</li>
+                                <li>Creating Hero sections</li>
+                                <li>Creating a custom contact form</li>
+                            </ul>
+                            <h3>Technologies used:</h3>
+                            <p><FaHtml5 /> <FaCss3 /> <IoLogoJavascript /> <FaReact /></p>
+                            <div className={styles.links}>
+                                <a href='https://moonlit-shortbread-27d65b.netlify.app'><button>Link</button></a>
+                                <a href='https://github.com/Matt-LaRochelle/carriage'><button>Code</button></a>
+                            </div>
+                        </div>
+                        <div className={styles.project_image}>
+                            <img src={ImgCarriage} alt='horse-carriage website' />
                         </div>
                     </div>
                 </SwiperSlide>
