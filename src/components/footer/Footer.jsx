640ce74a13bd18b79be990bc1ea4646274f0af0d
matthew <larochelle.matthew@gmail.com>
2023-04-06 22:54:57 -0700

    update navigation and icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './Footer.module.css';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 function Footer() {
 
@@ -10,8 +11,8 @@ function Footer() {
     return (
         <div className={styles.container}>
             <div className={styles.icons}>
-                <a href='https://github.com/Matt-LaRochelle'><FaGithub className={styles.icon} /><p className={styles.p}>GitHub</p></a>
-                <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><FaLinkedin className={styles.icon} /><p className={styles.p}>LinkedIn</p></a>
+                <a href='https://github.com/Matt-LaRochelle'><AiFillGithub className={styles.icon} /><p className={styles.p}>GitHub</p></a>
+                <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><AiFillLinkedin className={styles.icon} /><p className={styles.p}>LinkedIn</p></a>
             </div>
             <p>Copyright {year}</p>
         </div>
