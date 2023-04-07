9a386d0f03f1bc116e40e6380a6c86bcb1918d10
matthew <larochelle.matthew@gmail.com>
2023-04-06 17:41:41 -0700

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,8 +10,8 @@ function Footer() {
     return (
         <div className={styles.container}>
             <div className={styles.icons}>
-                <a href='https://github.com/Matt-LaRochelle'><FaGithub className={styles.icon} /></a>
-                <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><FaLinkedin className={styles.icon} /></a>
+                <a href='https://github.com/Matt-LaRochelle'><FaGithub className={styles.icon} /><p className={styles.p}>GitHub</p></a>
+                <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><FaLinkedin className={styles.icon} /><p className={styles.p}>LinkedIn</p></a>
             </div>
             <p>Copyright {year}</p>
         </div>
