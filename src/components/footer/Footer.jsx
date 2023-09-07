d9629836698835e40dbcad6b5a9dd3dd7d20e77b
matthew <larochelle.matthew@gmail.com>
2023-09-07 13:25:33 -0700

    Clean up footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
                 <a href='https://github.com/Matt-LaRochelle'><AiFillGithub className={styles.icon} /><p className={styles.p}>GitHub</p></a>
                 <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><AiOutlineLinkedin className={styles.icon} /><p className={styles.p}>LinkedIn</p></a>
             </div>
-            <p>Copyright {year}</p>
+            <p className={styles.copyright}>Copyright {year}</p>
         </div>
     );
 }
