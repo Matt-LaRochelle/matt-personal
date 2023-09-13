18c6c9246c5e0b4efec08e8fdfa8e3bd2a63897d
matthew <larochelle.matthew@gmail.com>
2023-09-13 12:18:10 -0700

    update footer and navbar styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,13 +9,13 @@ function Footer() {
 
     return (
         <div className="footerContainer">
+            <p className="year">Copyright {year}</p>
             <div className="icons">
                 <a href='https://github.com/Matt-LaRochelle'><p className="p">GitHub</p></a>
                 <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><p className="p">LinkedIn</p></a>
                 {/* <a href='https://github.com/Matt-LaRochelle'><AiFillGithub className={styles.icon} /><p className={styles.p}>GitHub</p></a>
                 <a href='https://www.linkedin.com/in/matthew-la-rochelle-565b41245/'><AiOutlineLinkedin className={styles.icon} /><p className={styles.p}>LinkedIn</p></a> */}
             </div>
-            <p className="year">Copyright {year}</p>
         </div>
     );
 }
