9913f41d217807d37017f3d0b07362cce017963d
matthew <larochelle.matthew@gmail.com>
2023-09-13 12:54:52 -0700

    update span color on homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './home.css'
-import Title from '../title/Title';
 import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
 
@@ -11,7 +10,7 @@ function Home() {
             <div className="text">
                 <h1>Hello,</h1>
                 <p>my name is Matt</p>
-                <p>I design and develop websites and web applications</p>
+                <p>I develop <span>websites</span> and <span>web applications</span></p>
             </div>
             <Footer />
         </div>
