76adf0151840f544b2c6ca40abb59cbe2536fd6e
matthew <larochelle.matthew@gmail.com>
2023-09-13 15:35:56 -0700

    Continue to format pages for navbar and footer

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './Tech.module.css'
+import './tech.css'
 import {FaReact, FaHtml5, FaCss3, FaBootstrap, FaGithub, FaDocker, FaNodeJs, FaPython} from 'react-icons/fa';
 import {IoLogoJavascript} from 'react-icons/io';
 import {SiMongodb, SiMysql, SiExpress} from 'react-icons/si';
@@ -8,66 +8,66 @@ import Footer from "../footer/Footer";
 
 function Tech() {
     return (
-        <div>
-            <div className={styles.tech}>
+            <div className="tech">
             <Navbar />
-                <h3 className={styles.title}>Skills</h3>
-                <div className={styles.stack}>
-                    <div className={styles.row1}>
-                        <div className={styles.item}>
-                            <FaHtml5 />
-                            <p>HTML</p>
+                <div className="techContainer">
+                    <h3>Skills</h3>
+                    <div className="stack">
+                        <div className="row1">
+                            <div className="item">
+                                <FaHtml5 />
+                                <p>HTML</p>
+                            </div>
+                            <div className="item">
+                                <FaCss3 />
+                                <p>CSS</p>
+                            </div>
+                            <div className="item">
+                                <IoLogoJavascript />
+                                <p>JavaScript</p>
+                            </div>
+                            <div className="item">
+                                <FaPython />
+                                <p>Python</p>
+                            </div>
+                            <div className="item">
+                                <SiMongodb />
+                                <p>MongoDB</p>
+                            </div>
+                            <div className="item">
+                                <SiExpress />
+                                <p>Express.js</p>
+                            </div>
+                            <div className="item">
+                                <FaNodeJs />
+                                <p>Node.js</p>
+                            </div>
+                            <div className="item">
+                                <FaReact />
+                                <p>React</p>
+                            </div>
+                            <div className="item">
+                                <SiMysql />
+                                <p>MySQL</p>
+                            </div>
+                            <div className="item">
+                                <FaBootstrap />
+                                <p>Bootstrap</p>
+                            </div>
+                            <div className="item">
+                                <FaGithub />
+                                <p>GitHub</p>
+                            </div>
+                            <div className="item">
+                                <FaDocker />
+                                <p>Docker</p>
+                            </div>
+                            
                         </div>
-                        <div className={styles.item}>
-                            <FaCss3 />
-                            <p>CSS</p>
-                        </div>
-                        <div className={styles.item}>
-                            <IoLogoJavascript />
-                            <p>JavaScript</p>
-                        </div>
-                        <div className={styles.item}>
-                            <FaPython />
-                            <p>Python</p>
-                        </div>
-                        <div className={styles.item}>
-                            <SiMongodb />
-                            <p>MongoDB</p>
-                        </div>
-                        <div className={styles.item}>
-                            <SiExpress />
-                            <p>Express.js</p>
-                        </div>
-                        <div className={styles.item}>
-                            <FaNodeJs />
-                            <p>Node.js</p>
-                        </div>
-                        <div className={styles.item}>
-                            <FaReact />
-                            <p>React</p>
-                        </div>
-                        <div className={styles.item}>
-                            <SiMysql />
-                            <p>MySQL</p>
-                        </div>
-                        <div className={styles.item}>
-                            <FaBootstrap />
-                            <p>Bootstrap</p>
-                        </div>
-                        <div className={styles.item}>
-                            <FaGithub />
-                            <p>GitHub</p>
-                        </div>
-                        <div className={styles.item}>
-                            <FaDocker />
-                            <p>Docker</p>
-                        </div>
-                        
                     </div>
                 </div>
-            </div>
             <Footer />
-        </div>
+            </div>
     );
 }
 
