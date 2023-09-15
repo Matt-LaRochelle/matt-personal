f1d8354e319b3f52cf264f6e6b605bd2adc3dfee
matthew <larochelle.matthew@gmail.com>
2023-09-15 14:59:21 -0700

    some small adjustments to background, contact

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -2,11 +2,13 @@ import './background.css'
 
 const Background = () => {
     return (
-        <div id="background">
-            <div className="outerCircle">
-                <div className="middleCircle">
-                    <div className="innerCircle">
+        <div className='background-box'>
+            <div id="background">
+                <div className="outerCircle">
+                    <div className="middleCircle">
+                        <div className="innerCircle">
 
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/projects/Windmill2.jsx
+++ src/components/projects/Windmill2.jsx
@@ -15,14 +15,22 @@ const Windmill2 = () => {
                 <p>A website for a horse back riding school</p>
                 <div className="textContent">
                     <div>
-                        <h3>The problem:</h3>
-                        <p>Shoshana had a decent amount of clients, but needed help growing her business, and thought that redesigning and developing her current website would help.</p>
-                        <h3>The project:</h3>
-                        <p>Build a simple, intuitive website which would display relevant and interesting information about the school.</p>
-                        <h3>Solutions:</h3>
-                        <p>I set about creating a website which had information about the teacher, what to expect at lessons, pricing, location, as well as information on the horses, and some motivational quotes featured on it. I kept the colors to a minimum and made sure that the content was easily digestable and organized coherently.</p>
-                        <h3>Future vision of the project:</h3>
-                        <p>I am currently implementing a backend and an administrators page to keep track of students, teachers, and horses, as well as give clients the ability to book lessons online.</p>
+                        <div className="projectProblem">
+                            <h3>The problem:</h3>
+                            <p>Shoshana had a decent amount of clients, but needed help growing her business, and thought that redesigning and developing her current website would help.</p>
+                        </div>
+                        <div className="projectProject">
+                            <h3>The project:</h3>
+                            <p>Build a simple, intuitive website which would display relevant and interesting information about the school.</p>
+                        </div>
+                        <div className="projectsolutions">
+                            <h3>Solutions:</h3>
+                            <p>I set about creating a website which had information about the teacher, what to expect at lessons, pricing, location, as well as information on the horses, and some motivational quotes featured on it. I kept the colors to a minimum and made sure that the content was easily digestable and organized coherently.</p>
+                        </div>
+                        <div className="projectFuture">
+                            <h3>Future vision of the project:</h3>
+                            <p>I am currently implementing a backend and an administrators page to keep track of students, teachers, and horses, as well as give clients the ability to book lessons online.</p>
+                        </div>
                     </div>
                     <div>
                         <h3>Technologies used:</h3>
