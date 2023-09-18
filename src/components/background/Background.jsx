9c8240a73587fd879f1f834a573e17c6b82b0496
matthew <larochelle.matthew@gmail.com>
2023-09-18 13:17:11 -0700

    background animation works!

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -3,15 +3,30 @@ import './background.css'
 const Background = () => {
     return (
         <div className='background-box'>
-            {/* <div id="background">
-                <div className="outerCircle">
-                    <div className="middleCircle">
-                        <div className="innerCircle">
-
-                        </div>
-                    </div>
+            <div className='container'>
+                <div className='bubbles'>
+                <span style={{ "--i": 11 }}></span>
+                    <span style={{ "--i": 12 }}></span>
+                    <span style={{ "--i": 24 }}></span>
+                    <span style={{ "--i": 10 }}></span>
+                    <span style={{ "--i": 14 }}></span>
+                    <span style={{ "--i": 23 }}></span>
+                    <span style={{ "--i": 18 }}></span>
+                    <span style={{ "--i": 16 }}></span>
+                    <span style={{ "--i": 19 }}></span>
+                    <span style={{ "--i": 20 }}></span>
+                    <span style={{ "--i": 22 }}></span>
+                    <span style={{ "--i": 25 }}></span>
+                    <span style={{ "--i": 18 }}></span>
+                    <span style={{ "--i": 21 }}></span>
+                    <span style={{ "--i": 15 }}></span>
+                    <span style={{ "--i": 13 }}></span>
+                    <span style={{ "--i": 26 }}></span>
+                    <span style={{ "--i": 17 }}></span>
+                    <span style={{ "--i": 13 }}></span>
+                    <span style={{ "--i": 28 }}></span>
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
