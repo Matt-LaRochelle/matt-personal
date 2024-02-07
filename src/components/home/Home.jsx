b11fc628d8aa0b3c63b6ae099be15cfae4634c47
matthew <larochelle.matthew@gmail.com>
2024-02-07 11:45:08 -0800

    home layout change

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './home.css'
 import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
@@ -11,12 +12,14 @@ function Home() {
             <Navbar />
             {/* <Background /> */}
             <div className="home__container">
-                <h1>Hello</h1>
-                <img src={me} alt="Matt La Rochelle" className="headshot" />
-                <div className="home__text">
-                    
-                    <h4>My name is Matt</h4>
-                    <p>I develop <span>websites</span> and <span>web applications</span></p>
+                <div className="home__element">
+                    <div className="home__text">
+                        <h1>Hello</h1>
+                        <h4>My name is Matt</h4>
+                        <p>I develop <span>websites</span> and <span>web applications</span></p>
+                        <Link to="/contact" ><button>Contact Me</button></Link>
+                    </div>
+                    <img src={me} alt="Matt La Rochelle" className="headshot" />
                 </div>
             </div>
             <Footer />
