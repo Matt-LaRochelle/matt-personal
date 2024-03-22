bc0f87c109b6db72cc3ae64b1b7a2ff15145a692
matthew <larochelle.matthew@gmail.com>
2024-03-22 16:54:27 -0700

    create better background picker

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -2,29 +2,37 @@ import './toggle.css'
 import { useState } from 'react'
 import { useBackgroundContext } from '../hooks/useBackgroundContext'
 
+import whisp from '../images/whisps.png'
+import morph from '../images/morph.png'
+import vista from '../images/vista.png'
+
 const Toggle = () => {
     const [title, setTitle] = useState("Whisps")
     const { background, dispatch } = useBackgroundContext()
   
   
-    const toggleBackground = () => {
+    const toggleBackground = (e) => {
       console.log('clicked')
-        if (background === 0) {
-          setTitle("Morphing Colors")
-          dispatch({ type: 'SET_COLORS' })
-        } else if (background === 1) {
-          setTitle("Vista which follows the time of day")
-          dispatch({ type: 'SET_SILHOUETTE' })
-        } else if (background === 2) {
+        if (e.target.name === "whisps") {
           setTitle("Whisps")
           dispatch({ type: 'SET_WHISP' })
+        } else if (e.target.name === "vista") {
+          setTitle("Vista-clock")
+          dispatch({ type: 'SET_SILHOUETTE' })
+        } else if (e.target.name === "morph") {
+          setTitle("Morphing colors")
+          dispatch({ type: 'SET_COLORS' })
         }
     }
 
     return (
         <div className="toggle">
-            <button onClick={toggleBackground} className="background-button">Toggle Background</button>
             <p className="background-title">{ title }</p>
+            <div className="background-picker-grid">
+              <img src={whisp} alt="whisp" name="whisps" onClick={toggleBackground} className="background-picker-grid-item" />
+              <img src={morph} alt="morph" name="morph" onClick={toggleBackground} className="background-picker-grid-item" />
+              <img src={vista} alt="vista" name="vista" onClick={toggleBackground} className="background-picker-grid-item" />
+            </div>
         </div>
     )
 }
